470aaa8312856355c52d0750c5eedd3c669196ae
b00lean <b00lean@b00lean.net>
2015-03-12 21:28:58 +0100

    Replaced REST library and added support for Fiat Exchange Rate Providers

---
+++ server_extensions_api/src/com/generalbytes/batm/server/extensions/IFiatExchangeRateProvider.java
@@ -0,0 +1,29 @@
+/*************************************************************************************
+ * Copyright (C) 2015 GENERAL BYTES s.r.o. All rights reserved.
+ *
+ * This software may be distributed and modified under the terms of the GNU
+ * General Public License version 2 (GPL2) as published by the Free Software
+ * Foundation and appearing in the file GPL2.TXT included in the packaging of
+ * this file. Please note that GPL2 Section 2[b] requires that all works based
+ * on this software must also be made publicly available under the terms of
+ * the GPL2 ("Copyleft").
+ *
+ * Contact information
+ * -------------------
+ *
+ * GENERAL BYTES s.r.o.
+ * Web      :  http://www.generalbytes.com
+ *
+ ************************************************************************************/
+package com.generalbytes.batm.server.extensions;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+
+public interface IFiatExchangeRateProvider {
+    public Set<String> getFiatCurrenciesFrom();
+    public Set<String> getFiatCurrenciesTo();
+
+    public BigDecimal getRate(String fromCurrency, String toCurrency);
+}
