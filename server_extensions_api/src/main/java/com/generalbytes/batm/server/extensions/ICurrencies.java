4fefaf525bb0e9d3c19827aeeedc8809daa3a412
rrunner7 <rrunner@cryptodiggers.eu>
2017-01-24 18:47:27 +0100

    Update ICurrencies.java

--- server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/ICurrencies.java
+++ server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/ICurrencies.java
@@ -31,6 +31,7 @@ public interface ICurrencies {
     public static final String NBT = "NBT";
     public static final String NXT = "NXT";
     public static final String START = "START";
+    public static final String TKN = "TKN";
     public static final String WDC = "WDC";
     public static final String DASH = "DASH";
 
