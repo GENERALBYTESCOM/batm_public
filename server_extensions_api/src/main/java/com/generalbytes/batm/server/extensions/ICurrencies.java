357e5e51f3cfb5708e0fb8952f84bbcd7f576d73
Alexander Garzon <venehosting@gmail.com>
2017-08-23 16:23:13 -0400

    Added POT to ICurrencies

--- server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/ICurrencies.java
+++ server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/ICurrencies.java
@@ -32,6 +32,7 @@ public interface ICurrencies {
     public static final String ICG = "ICG";
     public static final String NBT = "NBT";
     public static final String NXT = "NXT";
+    public static final String POT = "POT";
     public static final String START = "START";
     public static final String TKN = "TKN";
     public static final String WDC = "WDC";
