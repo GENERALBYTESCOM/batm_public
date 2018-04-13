1ab232a6f4d4d459b149875662a59009c042af77
Jon D <info@darkjon.de>
2018-04-13 15:13:30 +0200

    Update ICoinmarketcapAPI.java

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/dash/sources/coinmarketcap/ICoinmarketcapAPI.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/dash/sources/coinmarketcap/ICoinmarketcapAPI.java
@@ -23,10 +23,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("/v1/ticker/?limit=50")
+@Path("/v1/ticker")
 @Produces(MediaType.APPLICATION_JSON)
 public interface ICoinmarketcapAPI {
     @GET
-    @Path("/")
+    @Path("/?limit=50")
     CMCTicker[]  getTickers(@QueryParam("convert") String fiatCurrency);
 }
