f5cc8b3b3ef92b81c1e77874a44fc772bade5b7b
xdustinface <xdustinfacex@gmail.com>
2018-05-02 13:20:57 +0200

    Changed licence

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/smartcash/sources/smartcash/SmartCashRateSource.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/smartcash/sources/smartcash/SmartCashRateSource.java
@@ -4,23 +4,13 @@
 # Copyright 2018 dustinface
 # Created 29.04.2018
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+* This software may be distributed and modified under the terms of the GNU
+* General Public License version 2 (GPL2) as published by the Free Software
+* Foundation and appearing in the file GPL2.TXT included in the packaging of
+* this file. Please note that GPL2 Section 2[b] requires that all works based
+* on this software must also be made publicly available under the terms of
+* the GPL2 ("Copyleft").
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
 ## */
 
 package com.generalbytes.batm.server.extensions.extra.smartcash.sources.smartcash;
