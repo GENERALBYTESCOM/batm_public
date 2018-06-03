32499f46382cbd6f8596abaabc35e557aef27b21
sidhujag <sidhujag@hotmail.com>
2018-06-03 11:41:35 -0700

    updae accessors

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/dash/sources/coinmarketcap/CMCData.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/dash/sources/coinmarketcap/CMCData.java
@@ -1,14 +1,14 @@
 import java.math.BigDecimal;
 import com.generalbytes.batm.server.extensions.extra.dash.sources.coinmarketcap.CMCQuotes;
 public class CMCData {
-    private String id;
-    private String name;
-    private String symbol;
-    private String website_slug;
-    private BigDecimal rank;
-    private BigDecimal circulating_supply;
-    private BigDecimal total_supply;
-    private BigDecimal max_supply;
-    private CMCQuotes quotes;
-    private long last_updated;
+    public String id;
+    public String name;
+    public String symbol;
+    public String website_slug;
+    public BigDecimal rank;
+    public BigDecimal circulating_supply;
+    public BigDecimal total_supply;
+    public BigDecimal max_supply;
+    public CMCQuotes quotes;
+    public long last_updated;
 }
