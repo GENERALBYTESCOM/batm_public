5aaed37d990f2f16f452b2277e0a301be34db67d
Cláusio Barbosa <clausiobarbosa@gmail.com>
2018-06-15 15:43:27 -0300

    Fix SmartCash address validation

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/smartcash/SmartcashAddressValidator.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/smartcash/SmartcashAddressValidator.java
@@ -18,7 +18,7 @@
 package com.generalbytes.batm.server.extensions.extra.smartcash;
 
 import com.generalbytes.batm.server.coinutil.AddressFormatException;
-import com.generalbytes.batm.server.coinutil.Base58;
+import com.generalbytes.batm.server.coinutil.SmartCash.Base58;
 import com.generalbytes.batm.server.extensions.ExtensionsUtil;
 import com.generalbytes.batm.server.extensions.ICryptoAddressValidator;
 
