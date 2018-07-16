6bc863050da0672c4045cbfcfa661292ca09128c
michalsikora <michal@devhelp.pl>
2018-07-16 15:57:11 +0200

    implement rate source advanced for futurocoin

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/futurocoin/sources/yobit/YobitRateSource.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/futurocoin/sources/yobit/YobitRateSource.java
@@ -1,8 +1,7 @@
 package com.generalbytes.batm.server.extensions.extra.futurocoin.sources.yobit;
 
 import com.generalbytes.batm.server.extensions.Currencies;
-import com.generalbytes.batm.server.extensions.IRateSource;
-import com.generalbytes.batm.server.extensions.extra.futurocoin.sources.yobit.dto.YobitResponse;
+import com.generalbytes.batm.server.extensions.IRateSourceAdvanced;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
@@ -10,7 +9,7 @@ import java.util.Set;
 
 import si.mazi.rescu.RestProxyFactory;
 
-public class YobitRateSource implements IRateSource {
+public class YobitRateSource implements IRateSourceAdvanced {
 
     private IYobitAPI api;
 
@@ -34,16 +33,56 @@ public class YobitRateSource implements IRateSource {
 
     @Override
     public BigDecimal getExchangeRateLast(String cryptoCurrency, String fiatCurrency) {
-        if (!getFiatCurrencies().contains(fiatCurrency) || !getCryptoCurrencies().contains(cryptoCurrency)) {
-            return null;
-        }
-
-        YobitResponse response = api.getTicker(cryptoCurrency.toLowerCase(), fiatCurrency.toLowerCase());
-        return response.getFto_usd().getLast();
+        return isCurrencySupported(cryptoCurrency, fiatCurrency) ? api
+            .getTicker(cryptoCurrency.toLowerCase(), fiatCurrency.toLowerCase())
+            .getFto_usd()
+            .getLast() : null;
     }
 
     @Override
     public String getPreferredFiatCurrency() {
         return Currencies.USD;
+    }
+
+    @Override
+    public BigDecimal getExchangeRateForBuy(String cryptoCurrency, String fiatCurrency) {
+        return isCurrencySupported(cryptoCurrency, fiatCurrency) ? api
+            .getTicker(cryptoCurrency.toLowerCase(), fiatCurrency.toLowerCase())
+            .getFto_usd()
+            .getBuy() : null;
+    }
+
+    @Override
+    public BigDecimal getExchangeRateForSell(String cryptoCurrency, String fiatCurrency) {
+        return isCurrencySupported(cryptoCurrency, fiatCurrency) ? api
+            .getTicker(cryptoCurrency.toLowerCase(), fiatCurrency.toLowerCase())
+            .getFto_usd()
+            .getSell() : null;
+    }
+
+    @Override
+    public BigDecimal calculateBuyPrice(String cryptoCurrency, String fiatCurrency, BigDecimal cryptoAmount) {
+        final BigDecimal exchangeRateForBuy = getExchangeRateForBuy(cryptoCurrency, fiatCurrency);
+
+        if (exchangeRateForBuy != null) {
+            return exchangeRateForBuy.multiply(cryptoAmount);
+        }
+
+        return null;
+    }
+
+    @Override
+    public BigDecimal calculateSellPrice(String cryptoCurrency, String fiatCurrency, BigDecimal cryptoAmount) {
+        final BigDecimal exchangeRateForSell = getExchangeRateForSell(cryptoCurrency, fiatCurrency);
+
+        if (exchangeRateForSell != null) {
+            return exchangeRateForSell.multiply(cryptoAmount);
+        }
+
+        return null;
+    }
+
+    private boolean isCurrencySupported(String cryptoCurrency, String fiatCurrency) {
+        return getFiatCurrencies().contains(fiatCurrency) && getCryptoCurrencies().contains(cryptoCurrency);
     }
 }
