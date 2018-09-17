b1b1eefc4398de4f1ca55111dab4b535c2ae4d44
kelvinyin56 <42491336+kelvinyin56@users.noreply.github.com>
2018-09-17 16:49:27 +0800

    Update and rename LskBinanceAPI.java to ILskBinanceAPI.java

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/lisk/wallets/liskbinancewallet/ILskBinanceAPI.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/lisk/wallets/liskbinancewallet/ILskBinanceAPI.java
@@ -6,10 +6,10 @@ import java.io.IOException;
 
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
-public interface LskBinanceAPI { 	
+public interface ILskBinanceAPI { 	
     @GET
     @Path("/api/v3/account")
-    LskBinanceRespond getCryptoBalance(@HeaderParam("X-MBX-APIKEY") String apiKey, @QueryParam("recvWindow") String recvWindow, @QueryParam("timestamp") String timeStamp, @QueryParam("signature") String signature ) throws IOException;
+    LskBinanceResponse getCryptoBalance(@HeaderParam("X-MBX-APIKEY") String apiKey, @QueryParam("recvWindow") String recvWindow, @QueryParam("timestamp") String timeStamp, @QueryParam("signature") String signature ) throws IOException;
 
     @POST
     @Path("wapi/v1/withdraw.html")
