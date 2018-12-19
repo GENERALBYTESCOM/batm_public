b5b46991e58dee3088978f6eda6e23f51c7ab850
Stosh <schyczewski@gmail.com>
2018-12-18 19:48:48 -0500

    Copy Dash's CMC files and begin changes

---
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/sumcoin/sources/sumcoincash/ISumcoincashAPI.java
@@ -0,0 +1,39 @@
+/*************************************************************************************
+ * Copyright (C) 2014-2018 GENERAL BYTES s.r.o. All rights reserved.
+ *
+ * This software may be distributed and modified under the terms of the GNU
+ * General Public License version 2 (GPL2) as published by the Free Software
+ * Foundation and appearing in the file GPL2.TXT included in the packaging of
+ * this file. Please note that GPL2 Section 2[b] requires that all works based
+ * on this software must also be made publicly available under the terms of
+ * the GPL2 ("Copyleft").
+ *
+ * Contact information
+ * -------------------
+ *
+ * GENERAL BYTES s.r.o.
+ * Web      :  http://www.generalbytes.com
+ *
+ ************************************************************************************/
+package com.generalbytes.batm.server.extensions.extra.sumcoin.sources.sumcoincash;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Map;
+@Path("/sumprice")
+@Produces(MediaType.APPLICATION_JSON)
+public interface ISumcoincashAPI {
+    @GET
+    @Path("/ticker/{id}/")
+    SccTickerResponse  getTicker(@PathParam("id") Integer id, @QueryParam("convert") String fiatCurrency);
+
+    /**
+     * Method getListings() returns map which contain all suported crypto currencies.
+     * For all cryptocurrencies there is associated id.
+     *
+     * @return Map of cryptocurrencies
+     */
+    @GET
+    @Path("/listings")
+    Map<String, Object> getListings();
+}
---
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/sumcoin/sources/sumcoincash/SumcoincashRateSource.java
@@ -0,0 +1,162 @@
+/*************************************************************************************
+ * Copyright (C) 2014-2018 GENERAL BYTES s.r.o. All rights reserved.
+ *
+ * This software may be distributed and modified under the terms of the GNU
+ * General Public License version 2 (GPL2) as published by the Free Software
+ * Foundation and appearing in the file GPL2.TXT included in the packaging of
+ * this file. Please note that GPL2 Section 2[b] requires that all works based
+ * on this software must also be made publicly available under the terms of
+ * the GPL2 ("Copyleft").
+ *
+ * Contact information
+ * -------------------
+ *
+ * GENERAL BYTES s.r.o.
+ * Web      :  http://www.generalbytes.com
+ *
+ ************************************************************************************/
+package com.generalbytes.batm.server.extensions.extra.sumcoin.sources.sumcoincash;
+
+import com.generalbytes.batm.server.extensions.Currencies;
+import com.generalbytes.batm.server.extensions.IRateSource;
+import si.mazi.rescu.RestProxyFactory;
+
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+/**
+ * Created by kkyovsky on 11/29/17.
+ *
+ * Modified by sidhujag on 6/3/2018
+ */
+
+public class SumcoincashRateSource implements IRateSource {
+    /**
+     * Expiry of cache in seconds
+     */
+    private static final long CACHE_EXPIRY_TIME_DEFAULT = 600;
+    private ICoinmarketcapAPI api;
+    private String preferredFiatCurrency = Currencies.USD;
+    private static volatile long recentUnix = System.currentTimeMillis();
+    private static volatile Map<String,Integer> coinIDs;
+
+    public SumcoincashRateSource(String preferedFiatCurrency) {
+        this();
+        if (Currencies.EUR.equalsIgnoreCase(preferedFiatCurrency)) {
+            this.preferredFiatCurrency = Currencies.EUR;
+        }
+        if (Currencies.USD.equalsIgnoreCase(preferedFiatCurrency)) {
+            this.preferredFiatCurrency = Currencies.USD;
+        }
+        if (Currencies.CAD.equalsIgnoreCase(preferedFiatCurrency)) {
+            this.preferredFiatCurrency = Currencies.CAD;
+        }
+        if (Currencies.HKD.equalsIgnoreCase(preferedFiatCurrency)) {
+            this.preferredFiatCurrency = Currencies.HKD;
+        }
+    }
+
+    private SumcoincashRateSource() {
+        api = RestProxyFactory.createProxy(ISumcoincashAPI.class, "http://159.65.72.249/");
+        final long currentUnix = System.currentTimeMillis();
+        final long difference = currentUnix - recentUnix;
+        final long differenceInSeconds = TimeUnit.SECONDS.convert(difference, TimeUnit.MILLISECONDS);
+        if(coinIDs == null || coinIDs.isEmpty() || differenceInSeconds > CACHE_EXPIRY_TIME_DEFAULT) {
+            HashMap<String, Integer> localCoinIDs = new HashMap<>();
+            final Map<String, Object> listings = api.getListings();
+            if (listings != null && !listings.isEmpty()) {
+                final List<Object> dataList = (List<Object>) listings.get("data");
+                if(dataList != null && !dataList.isEmpty()) {
+                    for (Object dataobject : dataList) {
+                        final Map<String, Object> map = (Map<String, Object>) dataobject;
+                        final Integer id = (Integer) map.get("name");
+                        final String symbol = (String) map.get("code");
+                        if (!localCoinIDs.containsKey(symbol) && !localCoinIDs.containsValue(id)) {
+                            localCoinIDs.put(symbol, id);
+                        }
+                    }
+                }
+            }
+            CoinmarketcapRateSource.recentUnix = System.currentTimeMillis();
+            CoinmarketcapRateSource.coinIDs = localCoinIDs;
+        }
+    }
+
+    @Override
+    public Set<String> getCryptoCurrencies() {
+        Set<String> result = new HashSet<String>();
+        /*result.add(Currencies.BTC);
+        result.add(Currencies.SYS);
+        result.add(Currencies.BCH);
+        result.add(Currencies.BTX);
+        result.add(Currencies.LTC);
+        result.add(Currencies.ETH);
+        result.add(Currencies.DASH);
+        result.add(Currencies.XMR);
+        result.add(Currencies.PAC);
+        result.add(Currencies.POT);
+        result.add(Currencies.FLASH);
+        result.add(Currencies.BTCP);
+        result.add(Currencies.EFL);
+        result.add(Currencies.BSD);
+        result.add(Currencies.BTDX);
+        result.add(Currencies.MEC);
+        result.add(Currencies.BURST);
+        result.add(Currencies.DOGE);
+        result.add(Currencies.ECA);
+        result.add(Currencies.ANON);
+        result.add(Currencies.LSK);
+        result.add(Currencies.USDT);
+        result.add(Currencies.XZC);
+        result.add(Currencies.CLOAK);
+        result.add(Currencies.DAI); */
+		result.add(Currencies.SUM);
+
+        return result;
+    }
+
+    @Override
+    public Set<String> getFiatCurrencies() {
+        Set<String> result = new HashSet<String>();
+        result.add(Currencies.USD);
+        /*result.add(Currencies.CAD);
+        result.add(Currencies.EUR);
+        result.add(Currencies.HKD); */
+        return result;
+    }
+
+
+    @Override
+    public String getPreferredFiatCurrency() {
+        return Currencies.USD;
+    }
+
+    @Override
+    public BigDecimal getExchangeRateLast(String cryptoCurrency, String fiatCurrency) {
+        if (!getFiatCurrencies().contains(fiatCurrency)) {
+            return null;
+        }
+
+        Integer cryptoId = coinIDs.get(cryptoCurrency);
+        if (cryptoId == null) {
+            return null;
+        }
+        SccTickerResponse ticker = api.getTicker(cryptoId, fiatCurrency);
+        if (ticker == null) {
+            return null;
+        }
+        SccTickerData data = ticker.getData();
+        if (data == null) {
+            return null;
+        }
+        Map<String, SccTickerQuote> quotesByFiatCurrency = data.getQuotes();
+        if (quotesByFiatCurrency == null) {
+            return null;
+        }
+        SccTickerQuote quote = quotesByFiatCurrency.get(fiatCurrency);
+        if (quote == null) {
+            return null;
+        }
+        return quote.getPrice();
+    }
+}
