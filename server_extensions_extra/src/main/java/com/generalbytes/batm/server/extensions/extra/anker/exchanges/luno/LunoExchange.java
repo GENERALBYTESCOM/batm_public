da0e32f4362d60b71004d32bab4ac910ccede37c
Viktor <vtitch@gmail.com>
2019-02-14 13:15:18 +0100

    Update LunoExchange.java

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/anker/exchanges/luno/LunoExchange.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/anker/exchanges/luno/LunoExchange.java
@@ -10,7 +10,7 @@ import java.util.Set;
 import si.mazi.rescu.RestProxyFactory;
 
 
-public class LunoExchange implements IExchange {
+public class LunoExchange implements IRateSourceAdvanced, IExchangeAdvanced {
 
     private String preferredFiatCurrency = Currencies.ZAR;
     private String clientKey;
