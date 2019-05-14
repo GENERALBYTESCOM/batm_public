96402d6c14dfc8458c73296bd81d3646ab68169a
Victor Da Silva Vatcointech <50328273+VictorDaSilvaVat@users.noreply.github.com>
2019-05-14 12:17:44 +0200

    BATM Support for Goldcoin GLC

---
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/goldcoin/GoldCoinDefinition.java
@@ -0,0 +1,35 @@
+/*************************************************************************************
+ * Copyright (C) 2014-2019 GENERAL BYTES s.r.o. All rights reserved.
+ *
+ * This software may be distributed and modified under the terms of the GNU
+ * General Public License version 2 (GPL2) as published by the Free Software
+ * Foundation and appearing in the file GPL2.TXT included in the packaging of
+ * this file. Please note that GPL2 Section 2[b] requires that all works based
+ * on this software must also be made publicly available under the terms of
+ * the GPL2 ("Copyleft").
+ *
+ * Contact information
+ * -------------------
+ *
+ * GENERAL BYTES s.r.o.
+ * Web      :  http://www.generalbytes.com
+ *
+ ************************************************************************************/
+package com.generalbytes.batm.server.extensions.extra.goldcoin;
+
+import com.generalbytes.batm.server.extensions.CryptoCurrencyDefinition;
+import com.generalbytes.batm.common.currencies.CryptoCurrency;
+import com.generalbytes.batm.server.extensions.payment.IPaymentSupport;
+
+public class GoldCoinDefinition extends CryptoCurrencyDefinition{
+    private IPaymentSupport paymentSupport = new GoldCoinPaymentSupport();
+
+    public GoldCoinDefinition() {
+        super(CryptoCurrency.GLC.getCode(), "GoldCoin", "goldcoin","https://www.goldcoin.org/");
+    }
+
+    @Override
+    public IPaymentSupport getPaymentSupport() {
+        return paymentSupport;
+    }
+}
