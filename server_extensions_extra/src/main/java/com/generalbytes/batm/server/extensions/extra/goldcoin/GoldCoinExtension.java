0c733682dd05c9b4a6f2ba59f92f84b7010e8045
Victor Da Silva Vatcointech <50328273+VictorDaSilvaVat@users.noreply.github.com>
2019-05-17 12:55:45 +0200

    BATM Support for Goldcoin GLC

---
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/goldcoin/GoldCoinExtension.java
@@ -0,0 +1,98 @@
+/*************************************************************************************
+ * Copyright (C) 2014-2019 GENERAL BYTES s.r.o. All rights reserved.
+ *
+ * This software may be distributed and modified under the terms of the GNU
+ * General Public License version 2 (GPL2) as published by the Free Software
+ * Foundation and appearing in the file GPL2.TXT included in the packaging of
+ * this file. Please note that GPL2 Section 2[b] requires that all works based
+ * on this software must also be made publicly available under the terms of
+ * the GPL2 ("Copyleft").
+ *
+ * Contact information
+ * -------------------
+ *
+ * GENERAL BYTES s.r.o.
+ * Web      :  http://www.generalbytes.com
+ *
+ ************************************************************************************/
+package com.generalbytes.batm.server.extensions.extra.goldcoin;
+
+import com.generalbytes.batm.server.extensions.AbstractExtension;
+import com.generalbytes.batm.server.extensions.CryptoCurrencyDefinition;
+import com.generalbytes.batm.common.currencies.CryptoCurrency;
+import com.generalbytes.batm.server.extensions.ICryptoAddressValidator;
+import com.generalbytes.batm.server.extensions.ICryptoCurrencyDefinition;
+import com.generalbytes.batm.server.extensions.IPaperWalletGenerator;
+import com.generalbytes.batm.server.extensions.IWallet;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class GoldCoinExtension extends AbstractExtension {
+    private static final CryptoCurrencyDefinition DEFINITION = new GoldCoinDefinition();
+    public static final String CURRENCY = CryptoCurrency.GLC.getCode();
+
+    @Override
+    public String getName() {
+        return "BATM GoldCoin extra extension";
+    }
+
+    @Override
+    public IWallet createWallet(String walletLogin) {
+        if (walletLogin != null && !walletLogin.trim().isEmpty()) {
+            StringTokenizer st = new StringTokenizer(walletLogin, ":");
+            String walletType = st.nextToken();
+            if ("goldcoind".equalsIgnoreCase(walletType)) {
+                //"bitcoind:protocol:user:password:ip:port:accountname"
+
+                String protocol = st.nextToken();
+                String username = st.nextToken();
+                String password = st.nextToken();
+                String hostname = st.nextToken();
+                String port = st.nextToken();
+                String accountName = "";
+                if (st.hasMoreTokens()) {
+                    accountName = st.nextToken();
+                }
+
+
+                if (protocol != null && username != null && password != null && hostname != null && port != null && accountName != null) {
+                    String rpcURL = protocol + "://" + username + ":" + password + "@" + hostname + ":" + port;
+                    return new GoldCoinRPCWallet(rpcURL, accountName);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Set<String> getSupportedCryptoCurrencies() {
+        Set<String> result = new HashSet<String>();
+        result.add(CURRENCY);
+        return result;
+    }
+
+    @Override
+    public Set<ICryptoCurrencyDefinition> getCryptoCurrencyDefinitions() {
+        Set<ICryptoCurrencyDefinition> result = new HashSet<>();
+        result.add(DEFINITION);
+        return result;
+    }
+
+    @Override
+    public ICryptoAddressValidator createAddressValidator(String cryptoCurrency) {
+        if (CURRENCY.equalsIgnoreCase(cryptoCurrency)) {
+            return new GoldCoinAddressValidator();
+        }
+        return null;
+    }
+
+    @Override
+    public IPaperWalletGenerator createPaperWalletGenerator(String cryptoCurrency) {
+        if (CryptoCurrency.GLC.getCode().equalsIgnoreCase(cryptoCurrency)) {
+            return new GoldCoinWalletGenerator("qqqq", ctx);
+        }
+        return null;
+    }
+}
