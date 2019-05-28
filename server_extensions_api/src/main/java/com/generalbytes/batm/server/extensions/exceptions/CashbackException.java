cb85a2833ad6768cadcbdc0851302446469a69e3
kkyovsky <karel.kyovsky@generalbytes.com>
2019-05-28 18:48:01 +0200

    Added support for cashback

---
+++ server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/exceptions/CashbackException.java
@@ -0,0 +1,7 @@
+package com.generalbytes.batm.server.extensions.exceptions;
+
+public class CashbackException extends Exception {
+    public CashbackException(String message) {
+        super(message);
+    }
+}
