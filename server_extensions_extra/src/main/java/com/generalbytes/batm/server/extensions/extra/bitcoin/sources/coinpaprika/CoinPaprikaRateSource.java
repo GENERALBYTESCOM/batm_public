a042e9cc42effbc034f784c291f4d1ac07fab581
Sotiris Blad <26393434+sotblad@users.noreply.github.com>
2019-07-25 17:54:53 +0300

    Update CoinPaprikaRateSource.java

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/bitcoin/sources/coinpaprika/CoinPaprikaRateSource.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/bitcoin/sources/coinpaprika/CoinPaprikaRateSource.java
@@ -70,6 +70,7 @@ public class CoinPaprikaRateSource implements IRateSource {
         CRYPTOCURRENCIES.put(CryptoCurrency.MAX.getCode(), "max-maxcoin");
         CRYPTOCURRENCIES.put(CryptoCurrency.MEC.getCode(), "mec-megacoin");
         CRYPTOCURRENCIES.put(CryptoCurrency.MKR.getCode(), "mkr-maker");
+        CRYPTOCURRENCIES.put(CryptoCurrency.MUE.getCode(), "mue-monetaryunit");
         CRYPTOCURRENCIES.put(CryptoCurrency.NLG.getCode(), "nlg-gulden");
         CRYPTOCURRENCIES.put(CryptoCurrency.NULS.getCode(), "nuls-nuls");
         CRYPTOCURRENCIES.put(CryptoCurrency.NXT.getCode(), "nxt-nxt");
