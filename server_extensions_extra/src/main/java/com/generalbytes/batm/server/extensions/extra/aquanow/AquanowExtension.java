190b6cf534f215e7c90afd8e1e37427cc82e66c0
Pratik <pratik@aquanow.io>
2019-08-06 16:16:19 -0700

    code cleanup

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/aquanow/AquanowExtension.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/aquanow/AquanowExtension.java
@@ -5,7 +5,6 @@ import com.generalbytes.batm.common.currencies.FiatCurrency;
 import com.generalbytes.batm.server.extensions.*;
 import com.generalbytes.batm.server.extensions.FixPriceRateSource;
 import com.generalbytes.batm.server.extensions.extra.aquanow.sources.aquanow.AquanowRateSource;
-import com.generalbytes.batm.server.extensions.extra.aquanow.wallets.aquanowd.AquanowRPCWallet;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
@@ -16,46 +15,6 @@ public class AquanowExtension extends AbstractExtension{
     @Override
     public String getName() {
         return "BATM Aquanow extension";
-    }
-
-    @Override
-    public IWallet createWallet(String walletLogin) {
-        if (walletLogin !=null && !walletLogin.trim().isEmpty()) {
-            StringTokenizer st = new StringTokenizer(walletLogin,":");
-            String walletType = st.nextToken();
-
-            if ("aquanowd".equalsIgnoreCase(walletType)) {
-
-                String protocol = st.nextToken();
-                String username = st.nextToken();
-                String password = st.nextToken();
-                String hostname = st.nextToken();
-                String port = st.nextToken();
-                String accountName ="";
-                if (st.hasMoreTokens()) {
-                    accountName = st.nextToken();
-                }
-
-
-                if (protocol != null && username != null && password != null && hostname !=null && port != null && accountName != null) {
-                    String rpcURL = protocol +"://" + username +":" + password + "@" + hostname +":" + port;
-                    return new AquanowRPCWallet(rpcURL,accountName);
-                }
-            }
-            if ("aquanowdemo".equalsIgnoreCase(walletType)) {
-
-                String fiatCurrency = st.nextToken();
-                String walletAddress = "";
-                if (st.hasMoreTokens()) {
-                    walletAddress = st.nextToken();
-                }
-
-                if (fiatCurrency != null && walletAddress != null) {
-                    return new DummyExchangeAndWalletAndSource(fiatCurrency, CryptoCurrency.SMART.getCode(), walletAddress);
-                }
-            }
-        }
-        return null;
     }
 
     @Override
@@ -71,21 +30,7 @@ public class AquanowExtension extends AbstractExtension{
         if (sourceLogin != null && !sourceLogin.trim().isEmpty()) {
             StringTokenizer st = new StringTokenizer(sourceLogin,":");
             String exchangeType = st.nextToken();
-
-            if ("aquanowfix".equalsIgnoreCase(exchangeType)) {
-                BigDecimal rate = BigDecimal.ZERO;
-                if (st.hasMoreTokens()) {
-                    try {
-                        rate = new BigDecimal(st.nextToken());
-                    } catch (Throwable e) {
-                    }
-                }
-                String preferedFiatCurrency = FiatCurrency.USD.getCode();
-                if (st.hasMoreTokens()) {
-                    preferedFiatCurrency = st.nextToken().toUpperCase();
-                }
-                return new FixPriceRateSource(rate,preferedFiatCurrency);
-            }else if ("aquanowapi".equalsIgnoreCase(exchangeType)) {
+        if ("aquanowapi".equalsIgnoreCase(exchangeType)) {
                 String preferredFiatCurrency = FiatCurrency.USD.getCode();
                 if (st.hasMoreTokens()) {
                     preferredFiatCurrency = st.nextToken();
