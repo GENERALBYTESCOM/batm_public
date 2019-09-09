773aaee0b3fc89243228359ee2048106eff13144
Hatchpay <44335173+hatchpay@users.noreply.github.com>
2019-09-09 13:41:20 +1100

    Add Hatch (HATCH)

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/hatch/sources/coinmarketcap/CoinmarketcapRateSource.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/hatch/sources/coinmarketcap/CoinmarketcapRateSource.java
@@ -72,13 +72,14 @@ public class CoinmarketcapRateSource implements IRateSource {
         result.add(CryptoCurrency.BURST.getCode());
         result.add(CryptoCurrency.CLOAK.getCode());
         result.add(CryptoCurrency.DAI.getCode());
-        result.add(CryptoCurrency.HATCH.getCode());
+        result.add(CryptoCurrency.DASH.getCode());
         result.add(CryptoCurrency.DOGE.getCode());
         result.add(CryptoCurrency.ECA.getCode());
         result.add(CryptoCurrency.EFL.getCode());
         result.add(CryptoCurrency.ETH.getCode());
         result.add(CryptoCurrency.EURS.getCode());
         result.add(CryptoCurrency.FLASH.getCode());
+        result.add(CryptoCurrency.HATCH.getCode());
         result.add(CryptoCurrency.LSK.getCode());
         result.add(CryptoCurrency.LTC.getCode());
         result.add(CryptoCurrency.MEC.getCode());
