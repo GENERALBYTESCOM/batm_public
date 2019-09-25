dc857035b6bb99a7feaf8de8ddcdf3ca356c3850
Vlad Piliavsky <vladipil@gmail.com>
2019-09-25 15:23:53 +0300

    new version

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/bitcoin/exchanges/enigma/EnigmaExchange.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/bitcoin/exchanges/enigma/EnigmaExchange.java
@@ -4,15 +4,12 @@ import com.generalbytes.batm.common.currencies.CryptoCurrency;
 import com.generalbytes.batm.common.currencies.FiatCurrency;
 import com.generalbytes.batm.server.extensions.extra.bitcoin.exchanges.XChangeExchange;
 import org.knowm.xchange.ExchangeSpecification;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-// import java.math.BigDecimal;
+import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
 public class EnigmaExchange extends XChangeExchange {
-    // private static final Logger log = LoggerFactory.getLogger("batm.master.exchange.EnigmaExchange");
 
     public EnigmaExchange(String preferredFiatCurrency) {
         super(getDefaultSpecification(), preferredFiatCurrency);
@@ -66,8 +63,8 @@ public class EnigmaExchange extends XChangeExchange {
         System.out.println(rs.getExchangeRateLast("BTC", "USD"));
 
         // EnigmaExchange xch = new EnigmaExchange("", "", "USD");
-        // log.info("calculateSellPrice: " + xch.calculateSellPrice("BTC", "USD", new BigDecimal(1)).toString());
-        // log.info("getExchangeRateForSell: " + xch.getExchangeRateForSell("BTC", "USD").toString());
-        // log.info("getDepositAddress: " + xch.getDepositAddress("BTC"));
+        // System.out.println("getExchangeRateForSell" + xch.getExchangeRateLast("BTC", "USD"));
+        // System.out.println("calculateSellPrice: " + xch.calculateSellPrice("BTC", "USD", new BigDecimal(1)).toString());
+        // System.out.println("getDepositAddress: " + xch.getDepositAddress("BTC"));
     }
 }
