ecbbaa56fbaedecd26da69d86d3d1c6537765d16
pvyhnal <44365000+pvyhnal-generalbytes@users.noreply.github.com>
2020-04-27 19:12:20 +0200

    Changed General Bytes copyright to 2020
    Added a script for next year

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/dash/DashDefinition.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/dash/DashDefinition.java
@@ -1,5 +1,5 @@
 /*************************************************************************************
- * Copyright (C) 2014-2019 GENERAL BYTES s.r.o. All rights reserved.
+ * Copyright (C) 2014-2020 GENERAL BYTES s.r.o. All rights reserved.
  *
  * This software may be distributed and modified under the terms of the GNU
  * General Public License version 2 (GPL2) as published by the Free Software
