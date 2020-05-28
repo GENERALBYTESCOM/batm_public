eefdfbd7282fa2c04807411fe45ad9cb10ceff98
Marek Myslivec <marek.myslivec@generalbytes.com>
2020-05-28 19:10:26 +0200

    CF-45: Improve identity notes functionality - code review

--- server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/IIdentityNote.java
+++ server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/IIdentityNote.java
@@ -4,9 +4,29 @@ import java.util.Date;
 
 public interface IIdentityNote {
 
+    /**
+     * @return content of the note
+     */
     String getText();
-    Date getServerTime();
+
+    /**
+     * @return time of create the note
+     */
+    Date getCreateAt();
+
+    /**
+     * @return time of delete the note
+     */
+    Date getDeleteAt();
+
+    /**
+     * @return name of the user who created the note
+     */
     String getUserName();
+
+    /**
+     * @return true if note is marked as deleted
+     */
     boolean isDeleted();
 
 }
