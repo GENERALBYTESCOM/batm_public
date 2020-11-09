82f6f6c4c331dc14341411f16565e7ce78e87d2c
Premysl Vyhnal <44365000+pvyhnal-generalbytes@users.noreply.github.com>
2020-11-09 16:46:52 +0100

    BATM-1547 remove IApiAccess.isAuthenticated (null means not authenticated)

--- server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/IApiAccess.java
+++ server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/IApiAccess.java
@@ -3,7 +3,6 @@ package com.generalbytes.batm.server.extensions;
 import java.util.Collection;
 
 public interface IApiAccess {
-    boolean isAuthenticated();
     Collection<String> getTerminalSerialNumbers();
 
 }
