7f0b68563a0d9b2c408418f9c76265643bd4ac7b
Rudy Godoy <rudygodoyg@gmail.com>
2020-11-23 07:58:24 -0500

    feeRate parameter is also available for unique addresses wallets.

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/bitcoin/wallets/bitgo/v2/BitgoWalletWithUniqueAddresses.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/bitcoin/wallets/bitgo/v2/BitgoWalletWithUniqueAddresses.java
@@ -28,8 +28,12 @@ import si.mazi.rescu.HttpStatusIOException;
 public class BitgoWalletWithUniqueAddresses extends BitgoWallet implements IGeneratesNewDepositCryptoAddress {
     private static final Logger log = LoggerFactory.getLogger(BitgoWalletWithUniqueAddresses.class);
 
-    public BitgoWalletWithUniqueAddresses(String scheme, String host, int port, String token, String walletId, String walletPassphrase) {
-        super(scheme, host, port, token, walletId, walletPassphrase);
+    public BitgoWalletWithUniqueAddresses(String scheme, String host, int port, String token, String walletId, String walletPassphrase){
+      super(scheme, host, port, token, walletId, walletPassphrase, "");
+    }
+
+    public BitgoWalletWithUniqueAddresses(String scheme, String host, int port, String token, String walletId, String walletPassphrase, String feeRate) {
+        super(scheme, host, port, token, walletId, walletPassphrase, feeRate);
     }
 
     @Override
