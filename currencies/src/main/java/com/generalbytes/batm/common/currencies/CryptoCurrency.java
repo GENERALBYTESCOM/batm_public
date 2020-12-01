7936f6beb3eda88f2e57850ceff32dd869baf057
tomcki <tomek.nowicki.pl@gmail.com>
2020-12-02 00:52:40 +0100

    SnowGem rebranding to TENT, changed the currency name in CryptoCurrency

--- currencies/src/main/java/com/generalbytes/batm/common/currencies/CryptoCurrency.java
+++ currencies/src/main/java/com/generalbytes/batm/common/currencies/CryptoCurrency.java
@@ -98,7 +98,7 @@ public enum CryptoCurrency {
     WDC("WorldCoin"),
     XMR("Monero"),
     XORI("CHFToken"),
-    TENT("SnowGem"),
+    TENT("Tent"),
     XRP("Ripple"),
     XZC("Zcoin"),
     BAY("BitBayCoin"),
