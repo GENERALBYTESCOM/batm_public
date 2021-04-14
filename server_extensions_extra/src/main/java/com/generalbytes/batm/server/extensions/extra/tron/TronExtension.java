639ba1ffa06a41cae4ac69d92c94915737597f1e
Premysl Vyhnal <44365000+pvyhnal-generalbytes@users.noreply.github.com>
2021-04-14 18:12:14 +0200

    BPUB-1578 add TRC20 Tron tokens support

---
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/tron/TronExtension.java
@@ -0,0 +1,72 @@
+/*************************************************************************************
+ * Copyright (C) 2014-2020 GENERAL BYTES s.r.o. All rights reserved.
+ *
+ * This software may be distributed and modified under the terms of the GNU
+ * General Public License version 2 (GPL2) as published by the Free Software
+ * Foundation and appearing in the file GPL2.TXT included in the packaging of
+ * this file. Please note that GPL2 Section 2[b] requires that all works based
+ * on this software must also be made publicly available under the terms of
+ * the GPL2 ("Copyleft").
+ *
+ * Contact information
+ * -------------------
+ *
+ * GENERAL BYTES s.r.o.
+ * Web      :  http://www.generalbytes.com
+ *
+ ************************************************************************************/
+package com.generalbytes.batm.server.extensions.extra.tron;
+
+import com.generalbytes.batm.common.currencies.CryptoCurrency;
+import com.generalbytes.batm.server.extensions.AbstractExtension;
+import com.generalbytes.batm.server.extensions.ICryptoAddressValidator;
+import com.generalbytes.batm.server.extensions.IWallet;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class TronExtension extends AbstractExtension {
+    private static final Collection<String> supportedCryptoCurrencies = Collections.unmodifiableSet(new HashSet<String>() {{
+        add(CryptoCurrency.TRX.getCode());
+    }});
+
+
+    @Override
+    public String getName() {
+        return "BATM Tron extension";
+    }
+
+    @Override
+    public IWallet createWallet(String walletLogin, String tunnelPassword) {
+        if (walletLogin != null && !walletLogin.trim().isEmpty()) {
+            StringTokenizer st = new StringTokenizer(walletLogin, ":");
+            String walletType = st.nextToken();
+
+            if (walletType.startsWith("tridentTRC20_")) {
+                StringTokenizer wt = new StringTokenizer(walletType, "_");
+                wt.nextToken();
+                String tokenSymbol = wt.nextToken();
+                int tokenDecimalPlaces = Integer.parseInt(wt.nextToken());
+                String contractAddress = wt.nextToken();
+
+                String tronProApiKey = st.nextToken();
+                String hexPrivateKey = st.nextToken();
+                return new TRC20Wallet(tronProApiKey, hexPrivateKey, tokenSymbol, tokenDecimalPlaces, contractAddress);
+            }
+        }
+        return null;
+    }
+
+
+    @Override
+    public ICryptoAddressValidator createAddressValidator(String cryptoCurrency) {
+        if (!supportedCryptoCurrencies.contains(cryptoCurrency)) {
+            return null;
+        }
+
+        return new TronCryptoAddressValidator();
+    }
+
+}
