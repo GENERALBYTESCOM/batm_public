f9794ccf2bea0603f3c0a15e937316520bce73b3
Premysl Vyhnal <44365000+pvyhnal-generalbytes@users.noreply.github.com>
2021-05-28 13:31:57 +0200

    BPUB-1069 fix EtherScan - it now requires an api token

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/ethereum/etherscan/GetTransactionsResponse.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/ethereum/etherscan/GetTransactionsResponse.java
@@ -1,24 +1,24 @@
-package com.generalbytes.batm.server.extensions.extra.ethereum.etherscan.dto;
+package com.generalbytes.batm.server.extensions.extra.ethereum.etherscan;
 
 import java.util.List;
 
-public class GetTokenTransactionsResponse {
+public class GetTransactionsResponse {
     public String status;
     public String message;
     public List<Transaction> result;
-
 
     public static class Transaction {
         public String value;
         public String blockNumber;
         public String confirmations;
         public String contractAddress;
-        public String tokenSymbol;
         public String timestamp;
         public String hash;
         public String blockHash;
         public String from;
         public String to;
+        public String tokenSymbol;
         public String tokenDecimal;
     }
+
 }
--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/ethereum/etherscan/IEtherscanAPI.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/ethereum/etherscan/IEtherscanAPI.java
@@ -1,19 +1,31 @@
 package com.generalbytes.batm.server.extensions.extra.ethereum.etherscan;
 
-import com.generalbytes.batm.server.extensions.extra.ethereum.etherscan.dto.GetTokenTransactionsResponse;
+import si.mazi.rescu.ClientConfig;
+import si.mazi.rescu.RestProxyFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 
 @Path("")
 @Produces(MediaType.APPLICATION_JSON)
 public interface IEtherscanAPI {
 
+    static IEtherscanAPI create(String apiKey) {
+        ClientConfig config = new ClientConfig();
+        config.addDefaultParam(PathParam.class, "apikey", apiKey);
+        return RestProxyFactory.createProxy(IEtherscanAPI.class, "https://api.etherscan.io", config);
+    }
+
     @GET
-    @Path("/api")
-    GetTokenTransactionsResponse getTokenTransactions(@QueryParam("module") String module, @QueryParam("action") String action, @QueryParam("address") String address);
+    @Path("/api?module=account&action=tokentx&address={address}&apikey={apikey}")
+    GetTransactionsResponse getTokenTransactions(@PathParam("address") String address) throws EtherScanException, IOException;
+
+    @GET
+    @Path("/api?module=account&action=txlist&address={address}&apikey={apikey}")
+    GetTransactionsResponse getTransactions(@PathParam("address") String address) throws EtherScanException, IOException;
 
 }
