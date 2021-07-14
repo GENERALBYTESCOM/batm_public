604e42264039b7741eec1c6fa88ad28c89fd55f6
Premysl Vyhnal <44365000+pvyhnal-generalbytes@users.noreply.github.com>
2021-07-14 18:13:04 +0200

    BATM-1853 update Eclair wallet API to 0.6.0

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/lightningbitcoin/wallets/eclair/dto/SentInfo.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/lightningbitcoin/wallets/eclair/dto/SentInfo.java
@@ -17,15 +17,62 @@
  ************************************************************************************/
 package com.generalbytes.batm.server.extensions.extra.lightningbitcoin.wallets.eclair.dto;
 
+import java.util.List;
+
 public class SentInfo {
     public String id;
     public String paymentHash;
+    public String paymentType;
     public Status status;
     public Long amount;
     public Long createdAt;
-    public Long completedAt;
 
-    public enum Status {
-        PENDING, FAILED, SUCCEEDED;
+    public static class Status {
+        public Type type;
+        public List<Failure> failures;
+        public Long completedAt;
+        public Long feesPaid;
+        public String paymentPreimage;
+
+        public static class Failure {
+            public String failureType;
+            public String failureMessage;
+
+            @Override
+            public String toString() {
+                return "Failure{" +
+                    "failureType='" + failureType + '\'' +
+                    ", failureMessage='" + failureMessage + '\'' +
+                    '}';
+            }
+        }
+
+        public enum Type {
+            // see CustomTypeHints at https://github.com/ACINQ/eclair/blob/master/eclair-core/src/main/scala/fr/acinq/eclair/json/JsonSerializers.scala#L353
+            pending, failed, sent
+        }
+
+        @Override
+        public String toString() {
+            return "Status{" +
+                "type=" + type +
+                ", failures=" + failures +
+                ", completedAt=" + completedAt +
+                ", feesPaid=" + feesPaid +
+                ", paymentPreimage='" + paymentPreimage + '\'' +
+                '}';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "SentInfo{" +
+            "id='" + id + '\'' +
+            ", paymentHash='" + paymentHash + '\'' +
+            ", paymentType='" + paymentType + '\'' +
+            ", status=" + status +
+            ", amount=" + amount +
+            ", createdAt=" + createdAt +
+            '}';
     }
 }
