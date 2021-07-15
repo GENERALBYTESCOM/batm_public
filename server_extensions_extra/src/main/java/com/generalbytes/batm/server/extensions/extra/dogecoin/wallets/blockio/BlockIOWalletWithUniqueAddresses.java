a45eb14aacae3bb61ddc0dfdc522de43151191c6
Filip Ocelka <filip.ocelka@generalbytes.com>
2021-07-15 14:37:23 +0200

    BPUB-1630 BlockIO - removed deprecated API methods and wallet implementation

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/dogecoin/wallets/blockio/BlockIOWalletWithUniqueAddresses.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/dogecoin/wallets/blockio/BlockIOWalletWithUniqueAddresses.java
@@ -25,10 +25,10 @@ import si.mazi.rescu.HttpStatusIOException;
 
 import java.util.Random;
 
-public class BlockIOWalletWithClientSideSigningWithUniqueAddresses extends BlockIOWalletWithClientSideSigning implements IGeneratesNewDepositCryptoAddress {
-    private static final Logger log = LoggerFactory.getLogger(BlockIOWalletWithClientSideSigningWithUniqueAddresses.class);
+public class BlockIOWalletWithUniqueAddresses extends BlockIOWallet implements IGeneratesNewDepositCryptoAddress {
+    private static final Logger log = LoggerFactory.getLogger(BlockIOWalletWithUniqueAddresses.class);
 
-    public BlockIOWalletWithClientSideSigningWithUniqueAddresses(String apiKey, String pin, String priority, String fromLabel) {
+    public BlockIOWalletWithUniqueAddresses(String apiKey, String pin, String priority, String fromLabel) {
         super(apiKey, pin, priority, fromLabel);
     }
 
