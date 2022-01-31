ae59c52d5f32d3979ecb598a813344b2e40786d7
Premysl Vyhnal <44365000+pvyhnal-generalbytes@users.noreply.github.com>
2022-01-31 10:35:45 +0100

    BATM-2934 syscoin bech32 (sys1) addresses

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/syscoin/SyscoinAddressValidator.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/syscoin/SyscoinAddressValidator.java
@@ -19,9 +19,8 @@ package com.generalbytes.batm.server.extensions.extra.syscoin;
 
 import com.generalbytes.batm.server.coinutil.AddressFormatException;
 import com.generalbytes.batm.server.coinutil.Base58;
-import com.generalbytes.batm.server.extensions.ExtensionsUtil;
+import com.generalbytes.batm.server.coinutil.Bech32;
 import com.generalbytes.batm.server.extensions.ICryptoAddressValidator;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,14 +32,20 @@ public class SyscoinAddressValidator implements ICryptoAddressValidator {
             try {
                 Base58.decodeToBigInteger(address);
                 Base58.decodeChecked(address);
+                return true;
             } catch (AddressFormatException e) {
                 log.error("Error", e);
-                return false;
             }
-            return true;
-        }else{
-            return false;
+        } else if (address.startsWith("sys1")) {
+            try {
+                Bech32.decodeAddress(address);
+                return true;
+            } catch (AddressFormatException e) {
+                log.error("Error", e);
+            }
         }
+        log.warn("syscoin address not valid: {}", address);
+        return false;
     }
 
     @Override
---
+++ server_extensions_extra/src/test/java/com/generalbytes/batm/server/extensions/extra/syscoin/SyscoinAddressValidatorTest.java
@@ -0,0 +1,19 @@
+package com.generalbytes.batm.server.extensions.extra.syscoin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SyscoinAddressValidatorTest {
+    private final SyscoinAddressValidator validator = new SyscoinAddressValidator();
+
+    @Test
+    public void isAddressValid() {
+        assertTrue(validator.isAddressValid("SSt4EYRJGeQv6vtQy3BgH2NbyFpHPZL27P"));
+        assertTrue(validator.isAddressValid("SWu38cLXzEdxwTCiCKLsmUEMu6nh9ext57"));
+        assertFalse(validator.isAddressValid("SSt4EYRJGeQv6xxxxxxxxxxxxxxxxxxxxx"));
+
+        assertTrue(validator.isAddressValid("sys1q6dxdsytx6r2jerzm3g8zsdhgrs4s585y63gzm5"));
+        assertFalse(validator.isAddressValid("sys1qqqqqq"));
+    }
+}
