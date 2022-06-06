20b3a187840065d5a920ef5f09c6f73a44f992f3
Premysl Vyhnal <44365000+pvyhnal-generalbytes@users.noreply.github.com>
2022-06-06 23:01:45 +0200

    BPUB-1819 notification support in extensions

--- server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/INotificationListener.java
+++ server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/INotificationListener.java
@@ -17,119 +17,222 @@
  ************************************************************************************/
 package com.generalbytes.batm.server.extensions;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Instances implementing this interface could be registered using {@link IExtensionContext#addNotificationListener}
  * to receive various events about transactions, terminals, etc. The same events are used to trigger notifications
  * configured in Notification Policies in CAS, but extensions using this interface receive all the notifications
- * regardless of the notifications, parameters or actions configured in CAS.
+ * regardless of any configuration in Notification Policies in CAS.
+ *
+ * For most of the notifications the listener receives terminal serial number and transaction RID or identity public ID etc.,
+ * and the listener can fetch additional information (terminal Location, transaction details etc.) using {@link IExtensionContext}.
  */
 public interface INotificationListener {
 
-    default void transactionSuccess(INotificationDetails notificationDetails){}
+    default void transactionSuccess(String terminalSerialNumber, BigDecimal cashAmount, String cashCurrency, String transactionRemoteId) {}
 
-    default void transactionFailed(INotificationDetails notificationDetails){}
+    default void transactionFailed(String terminalSerialNumber, BigDecimal cashAmount, String cashCurrency, String transactionRemoteId) {}
 
-    default void walletCryptoBalanceLow(INotificationDetails notificationDetails){}
+    /**
+     * Transaction was successfully inserted into an Output Queue
+     */
+    default void transactionQueued(String terminalSerialNumber, BigDecimal cashAmount, String cashCurrency, String transactionRemoteId) {}
 
-    default void exchangeCryptoBalanceLow(INotificationDetails notificationDetails){}
+    default void cashbackCreated(String terminalSerialNumber, BigDecimal cashAmount, String cashCurrency) {}
 
-    default void cashHigh(INotificationDetails notificationDetails){}
+    default void invalidPaymentReceived(String terminalSerialNumber, BigDecimal amount, String cryptoCurrency, String fromAddress, String toAddress, String transactionRemoteId) {}
 
-    default void customerEnrolled(INotificationDetails notificationDetails){}
+    /**
+     * Note: the "high" and "low" listener callbacks are triggered regardless of any configuration in Notification Policies,
+     * you might need to implement your own logic to decide the low and high amount in your listener.
+     */
+    default void walletCryptoBalanceLow(String terminalSerialNumber, BigDecimal balance, String cryptoCurrency) {}
 
-    default void terminalOffline(INotificationDetails notificationDetails){}
+    default void walletCryptoBalanceHigh(String terminalSerialNumber, BigDecimal balance, String cryptoCurrency) {}
 
-    default void terminalWentOnlineAfterOffline(INotificationDetails notificationDetails){}
+    default void exchangeCryptoBalanceLow(String terminalSerialNumber, BigDecimal balance, String cryptoCurrency) {}
 
-    default void stackerManipulation(INotificationDetails notificationDetails){}
+    default void exchangeCryptoBalanceHigh(String terminalSerialNumber, BigDecimal balance, String cryptoCurrency) {}
 
-    default void terminalError(INotificationDetails notificationDetails){}
+    default void exchangeFiatBalanceLow(String terminalSerialNumber, BigDecimal balance, String cryptoCurrency) {}
 
-    default void transactionCashLimitReached(INotificationDetails notificationDetails){}
+    default void recyclerCashLow(String terminalSerialNumber, BigDecimal recyclerTotalAmount, String currency) {}
 
-    default void cashLow(INotificationDetails notificationDetails){}
+    /**
+     * Current total cash amount in a cashbox.
+     * <p>
+     * Note: the "high" and "low" listener callbacks are triggered regardless of any configuration in Notification Policies,
+     * you might need to implement your own logic to decide the low and high amount in your listener.
+     */
+    default void cashHigh(String terminalSerialNumber, BigDecimal cashboxTotalAmount, String currency, String cashboxName) {}
 
-    default void cashCountHigh(INotificationDetails notificationDetails){}
+    default void cashLow(String terminalSerialNumber, BigDecimal cashboxTotalAmount, String currency, String cashboxName) {}
 
-    default void cashCountLow(INotificationDetails notificationDetails){}
+    /**
+     * Current banknote count in a cashbox
+     */
+    default void cashCountHigh(String terminalSerialNumber, String cashboxName, int cashboxBanknoteCount) {}
 
-    default void invalidPaymentReceived(INotificationDetails notificationDetails){}
+    default void cashCountLow(String terminalSerialNumber, String cashboxName, int cashboxBanknoteCount) {}
 
-    default void clearedBalance(INotificationDetails notificationDetails){}
+    default void stackerFull(String terminalSerialNumber) {}
 
-    default void transactionSupplyLimitReached(INotificationDetails notificationDetails){}
+    default void stackerIn(String terminalSerialNumber) {}
 
-    default void rateSourceNotAvailable(INotificationDetails notificationDetails){}
+    default void stackerOut(String terminalSerialNumber) {}
 
-    default void countersShortCleared(INotificationDetails notificationDetails){}
+    default void stackerOutOutsideOpeningHours(String terminalSerialNumber) {}
 
-    default void watchlistScanBan(INotificationDetails notificationDetails){}
+    default void stackerOutForPeriod(String terminalSerialNumber, Date lastStackerOutEventTime) {}
 
-    default void exchangeFiatBalanceLow(INotificationDetails notificationDetails){}
+    default void doorSensor(String terminalSerialNumber, String eventData) {}
 
-    default void stackerOutForPeriod(INotificationDetails notificationDetails){}
+    /**
+     * There was attempt to perform transaction with machine door opened
+     */
+    default void possibleFraud(String terminalSerialNumber) {}
 
-    default void doorSensor(INotificationDetails notificationDetails){}
+    default void qrCodeStickerDetected(String terminalSerialNumber, String qrCodeContent) {}
 
-    default void stackerOutOutsideOpeningHours(INotificationDetails notificationDetails){}
+    /**
+     * Alarm has been armed or disarmed
+     *
+     * @param alarmStatus new alarm status (ARMED, DISARMED)
+     */
+    default void alarmStatus(String terminalSerialNumber, String alarmStatus) {}
 
-    default void transactionScoring(INotificationDetails notificationDetails){}
+    default void alarmPinRejected(String terminalSerialNumber) {}
 
-    default void qrCodeStickerDetected(INotificationDetails notificationDetails){}
+    default void alarmPinAccepted(String terminalSerialNumber) {}
 
-    default void alarmStatus(INotificationDetails notificationDetails){}
+    default void alarmTriggered(String terminalSerialNumber) {}
 
-    default void alarmPinRejected(INotificationDetails notificationDetails){}
+    /**
+     * Terminal recognized that cassette/s were removed from the bill dispenser
+     */
+    default void dispenserCassetteOut(String terminalSerialNumber, String cassetteInfo) {}
 
-    default void alarmPinAccepted(INotificationDetails notificationDetails){}
+    /**
+     * Terminal recognized that cassette/s were inserted into the bill dispenser
+     */
+    default void dispenserCassetteIn(String terminalSerialNumber, String cassetteInfo) {}
 
-    default void alarmTriggered(INotificationDetails notificationDetails){}
+    /**
+     * Terminal recognized cassette position change during restart
+     */
+    default void dispenserCassettePositionChange(String terminalSerialNumber) {}
 
-    default void transactionScoringSuspicious(INotificationDetails notificationDetails){}
+    /**
+     * User have changed cassette position and confirmed that banknote denomination and count written in cassette config are correct
+     */
+    default void userConfirmedDispenserCassettePositionChange(String terminalSerialNumber) {}
 
-    default void ipWhitelistRejected(INotificationDetails notificationDetails){}
+    default void dispenserInitFailed(String terminalSerialNumber) {}
 
-    default void dispenserCassetteManipulation(INotificationDetails notificationDetails){}
+    /**
+     * Dispenser reported that a cassette is empty when it attempted to dispense
+     */
+    default void cashboxEmptyError(String terminalSerialNumber, String emptyCassetteName) {}
 
-    default void acceptorAcceptanceProblem(INotificationDetails notificationDetails){}
+    /**
+     * A customer have trouble finishing a transaction few times in row
+     */
+    default void transactionAttemptsReached(String terminalSerialNumber) {}
 
-    default void cashbackCreated(INotificationDetails notificationDetails){}
+    /**
+     * Acceptance problem acceptor could not accept banknote few times in a row
+     */
+    default void acceptorAcceptanceProblem(String terminalSerialNumber) {}
 
-    default void cashCollectionMissed(INotificationDetails notificationDetails){}
+    /**
+     * Machine is inactive for long time period
+     */
+    default void machineInactivity(String terminalSerialNumber) {}
 
-    default void dispenserInitFailed(INotificationDetails notificationDetails){}
+    default void ipWhitelistRejected(String terminalSerialNumber, String oldIpWhitelist, String newIpWhitelist) {}
 
-    default void transactionAttemptsReached(INotificationDetails notificationDetails){}
+    default void ipWhitelistAutoUpdated(String terminalSerialNumber, String oldIpWhitelist, String newIpWhitelist) {}
 
-    default void machineInactivity(INotificationDetails notificationDetails){}
+    /**
+     * Server rejected terminal due to certificate fingerprint mismatch
+     */
+    default void certificateFingerprintMismatch(String terminalSerialNumber) {}
 
-    default void possibleFraud(INotificationDetails notificationDetails){}
+    default void terminalOffline(String terminalSerialNumber, Date lastPing) {}
 
-    default void blacklistedAddressUsed(INotificationDetails notificationDetails){}
+    default void terminalWentOnlineAfterOffline(String terminalSerialNumber, Date onlineSince) {}
 
-    default void watchedIdentityTransaction(INotificationDetails notificationDetails){}
+    default void terminalError(String terminalSerialNumber, List<String> terminalErrorNames) {}
 
-    default void recyclerCashLow(INotificationDetails notificationDetails){}
+    default void terminalRecoveredFromError(String terminalSerialNumber, List<String> recoveredFromErrorNames) {}
 
-    default void terminalRecoveredFromError(INotificationDetails notificationDetails){}
+    default void rateSourceNotAvailable(String terminalSerialNumber, List<String> notAvailableRateSources) {}
 
-    default void transactionQueued(INotificationDetails notificationDetails){}
+    default void rateSourceAvailable(String terminalSerialNumber, List<String> newlyAvailableRateSources) {}
 
-    default void ipWhitelistAutoUpdated(INotificationDetails notificationDetails){}
+    default void clearedBalance(String terminalSerialNumber) {}
 
-    default void certificateFingerprintMismatch(INotificationDetails notificationDetails){}
+    default void countersShortCleared(String terminalSerialNumber, String cashCollectionPublicId) {}
 
-    default void cashboxEmptyError(INotificationDetails notificationDetails){}
+    /**
+     * Planned cash collection is missing
+     */
+    default void cashCollectionMissed(String terminalSerialNumber, LocalDate missedCashCollectionDate) {}
 
-    default void watchlistScanIdentityMatches(INotificationDetails notificationDetails){}
+    default void customerEnrolled(String terminalSerialNumber, String identityPublicId) {}
 
-    default void lifetimeIdentityVolumeReached(INotificationDetails notificationDetails){}
+    default void customerAgreedToMarketingOptIn(String terminalSerialNumber, String identityPublicId) {}
 
-    default void customerAgreedToMarketingOptIn(INotificationDetails notificationDetails){}
+    default void blacklistedAddressUsed(String terminalSerialNumber, String cryptoCurrency, String address, String identityPublicId) {}
 
-    default void walletCryptoBalanceHigh(INotificationDetails notificationDetails){}
+    /**
+     * Watched identity performed a transaction
+     */
+    default void watchedIdentityTransaction(String terminalSerialNumber, String identityPublicId, String transactionRemoteId) {}
 
-    default void exchangeCryptoBalanceHigh(INotificationDetails notificationDetails){}
+    /**
+     * name found on a watchlist
+     */
+    default void watchlistScanBan(String terminalSerialNumber) {}
 
-    default void travelRuleSubmissionFailed(INotificationDetails notificationDetails){}
+    /**
+     * There is a match for Identity on a Watchlist
+     */
+    default void watchlistScanIdentityMatches(String identityPublicId) {}
+
+    default void lifetimeIdentityVolumeReached(String terminalSerialNumber, BigDecimal lifetimeVolume, String cashCurrency, BigDecimal preConditionAmount, String identityPublicId) {}
+
+    default void transactionSupplyLimitReached(String terminalSerialNumber) {}
+
+    default void transactionCashLimitReached(String terminalSerialNumber, BigDecimal cashAmount, String cashCurrency, String identityPublicId, String limitName, BigDecimal resultingLimit, Map<String, BigDecimal> limitsReached) {}
+
+    /**
+     * Transaction scoring provider flagged this address as high risk and blocked it
+     */
+    default void highRiskAddress(String terminalSerialNumber, String address, String cryptoCurrency, String identityPublicId) {}
+
+    /**
+     * Transaction scoring provider flagged this incoming transaction as high risk and blocked it
+     */
+    default void highRiskTransaction(String terminalSerialNumber, String transactionHash, String cryptoCurrency, String identityPublicId) {}
+
+    /**
+     * Transaction scoring provider flagged this address as suspicious (not blocked)
+     */
+    default void suspiciousAddress(String terminalSerialNumber, String address, String cryptoCurrency, String identityPublicId) {}
+
+    /**
+     * Transaction scoring provider flagged this incoming transaction as suspicious (not blocked)
+     */
+    default void suspiciousTransaction(String terminalSerialNumber, String transactionHash, String cryptoCurrency, String identityPublicId) {}
+
+    /**
+     * Travel Rule transaction submission failed
+     */
+    default void travelRuleSubmissionFailed(String terminalSerialNumber, String identityPublicId, String response, BigDecimal cryptoAmount, String cryptoCurrency, String cryptoAddress) {}
 }
