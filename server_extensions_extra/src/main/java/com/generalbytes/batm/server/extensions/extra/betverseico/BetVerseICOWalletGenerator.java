45091aced98b78bfa64bc10c24f5ca0e25534f1d
Enrique Souza <enriquesouza@live.com>
2022-06-29 21:39:53 +0200

    url presale

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/betverseico/BetVerseICOWalletGenerator.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/betverseico/BetVerseICOWalletGenerator.java
@@ -67,7 +67,8 @@ public class BetVerseICOWalletGenerator implements IPaperWalletGenerator {
             }
         }
 
-        return new BetVerseICOPaperWallet(content, address, mnemonic, messageText, "application/zip", "zip",
+        return new BetVerseICOPaperWallet(content, "https://betverse.ch/presale/" + address, mnemonic, messageText,
+                "application/zip", "zip",
                 cryptoCurrency);
     }
 
