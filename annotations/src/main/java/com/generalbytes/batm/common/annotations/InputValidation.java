43a6019c6366d1cb9c5e85c43dafe6de6a9373f4
Silady <maros.silady@generalbytes.com>
2023-04-13 13:09:03 +0200

    BATM-4839 - Create annotations module

--- annotations/src/main/java/com/generalbytes/batm/common/annotations/InputValidation.java
+++ annotations/src/main/java/com/generalbytes/batm/common/annotations/InputValidation.java
@@ -8,8 +8,6 @@ import java.lang.annotation.Target;
 /**
  * This annotation is used to specify input validation on fields and parameters.
  * It allows developers to define a method for input validation at runtime.
- * The validation method should be implemented separately and should return a boolean value
- * indicating if the input is valid (true) or not (false).
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER})
