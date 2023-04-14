a8ced08730dc6d1a3d64480df23c545b8c1fb987
Maroš Silady <38668012+SMaros@users.noreply.github.com>
2023-04-14 15:44:48 +0200

    BATM-4839 create annotations module (#802)

---
+++ annotations/src/main/java/com/generalbytes/batm/validation/InputValidation.java
@@ -0,0 +1,16 @@
+package com.generalbytes.batm.validation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is used to specify input validation on fields and parameters.
+ * It allows developers to define a method for input validation at runtime.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+public @interface InputValidation {
+    String method() default "";
+}
