2027153e1ed45d4ef9a105d320b86ac4cba7bea9
Maros Silady <maros.silady@generalbytes.com>
2023-10-12 18:07:12 +0200

    LC-250 - BitBuy exchange shows the wrong rate

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/bitcoin/exchanges/bitbuy/BitbuyExchange.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/bitcoin/exchanges/bitbuy/BitbuyExchange.java
@@ -150,11 +150,7 @@ public class BitbuyExchange implements IExchangeAdvanced, IRateSourceAdvanced {
     @Override
     public BigDecimal getExchangeRateForSell(String cryptoCurrency, String fiatCurrency) {
         BigDecimal rateSourceCryptoVolume = getRateSourceCryptoVolume(cryptoCurrency);
-        BigDecimal result = calculateSellPrice(cryptoCurrency, fiatCurrency, rateSourceCryptoVolume);
-        if (result != null) {
-            return result.divide(rateSourceCryptoVolume, 2, RoundingMode.DOWN);
-        }
-        return null;
+        return calculateSellPrice(cryptoCurrency, fiatCurrency, rateSourceCryptoVolume);
     }
 
     @Override
