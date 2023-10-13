1d8bc8df97f448ff1f82f37cc8a82e3e1dbf7b93
Maroš Silady <38668012+SMaros@users.noreply.github.com>
2023-10-13 16:40:26 +0200

    LC-249 - Adding states and territories for Australian locations (#853)

---
+++ server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/CountryAustralia.java
@@ -0,0 +1,46 @@
+package com.generalbytes.batm.server.extensions;
+
+/**
+ * Australia province identifiers.
+ * <p>
+ * Usage e.g.:
+ * CountryAustralia.AU-NSW.getProvinceName()
+ * CountryAustralia.valueOf("AU-NSW").getProvinceName()
+ */
+public enum CountryAustralia {
+
+    AU_NSW("AU-NSW", "New South Wales"),
+    AU_QLD("AU-QLD", "Queensland"),
+    AU_SA("AU-SA", "South Australia"),
+    AU_TAS("AU-TAS", "Tasmania"),
+    AU_VIC("AU-VIC", "Victoria"),
+    AU_WA("AU-WA", "Western Australia"),
+    AU_ACT("AU-ACT", "Australian Capital Territory"),
+    AU_NT("AU-NT", "Northern Territory");
+
+    private final String iso;
+
+    private final String provinceName;
+
+    /**
+     * Private constructor.
+     */
+    CountryAustralia(String iso, String provinceName) {
+        this.iso = iso;
+        this.provinceName = provinceName;
+    }
+
+    /**
+     * ISO 3166-2 code of the province (2 digits).
+     */
+    public String getIso() {
+        return iso;
+    }
+
+    /**
+     * English province/territory name officially used by the ISO 3166 Maintenance Agency (ISO 3166/MA).
+     */
+    public String getProvinceName() {
+        return provinceName;
+    }
+}
