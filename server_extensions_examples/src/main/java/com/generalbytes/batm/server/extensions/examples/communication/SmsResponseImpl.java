7a0107304afab89f309380ceb31383a978e041cd
kkyovsky <karel.kyovsky@generalbytes.com>
2023-12-06 11:41:29 +0100

    BATM-5682 - Extensions examples should be in separate module

---
+++ server_extensions_examples/src/main/java/com/generalbytes/batm/server/extensions/examples/communication/SmsResponseImpl.java
@@ -0,0 +1,40 @@
+package com.generalbytes.batm.server.extensions.examples.communication;
+
+import com.generalbytes.batm.server.extensions.communication.ISmsErrorResponse;
+import com.generalbytes.batm.server.extensions.communication.ISmsResponse;
+
+import java.math.BigDecimal;
+
+public class SmsResponseImpl implements ISmsResponse {
+    private final String sid;
+    private final ResponseStatus status;
+    private final BigDecimal price;
+    private final ISmsErrorResponse errorResponse;
+
+    public SmsResponseImpl(String sid, ResponseStatus status, BigDecimal price, ISmsErrorResponse errorResponse) {
+        this.sid = sid;
+        this.status = status;
+        this.price = price;
+        this.errorResponse = errorResponse;
+    }
+
+    @Override
+    public String getSid() {
+        return sid;
+    }
+
+    @Override
+    public ResponseStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Override
+    public ISmsErrorResponse getErrorResponse() {
+        return errorResponse;
+    }
+}
