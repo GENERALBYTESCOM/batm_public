d5c8068589df780d6c85668969ef06f169d73753
Dominik Roček <dominik.rocek@generalbytes.com>
2023-12-20 12:15:50 +0100

    BATM-5730: sending Custom Field ID to extension

--- server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/quiz/QuestionAnswer.java
+++ server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/quiz/QuestionAnswer.java
@@ -17,12 +17,27 @@
  ************************************************************************************/
 package com.generalbytes.batm.server.extensions.quiz;
 
+import com.generalbytes.batm.server.extensions.customfields.CustomFieldDefinition;
+import com.generalbytes.batm.server.extensions.customfields.CustomFieldDefinitionType;
+
 /**
  * An object representing the question and answer data in the {@link QuizResult}.
  */
 public class QuestionAnswer {
 
+    /**
+     * ID of {@link CustomFieldDefinition} representing a question.
+     * May be null if the Custom Field has been deleted on the server.
+     */
+    private Long customFieldDefinitionId;
+
     private String question;
+
+    /**
+     * ID of {@link CustomFieldDefinition.Element} representing an answer for a type {@link CustomFieldDefinitionType#DROPDOWN} and {@link CustomFieldDefinitionType#RADIO_BTN}.
+     * May be null if it is not one of the above types or if the Custom Field Element has been deleted on the server.
+     */
+    private Long customFieldElementId;
 
     private String answer;
 
@@ -30,9 +45,19 @@ public class QuestionAnswer {
 
     }
 
-    public QuestionAnswer(String question, String answer) {
+    public QuestionAnswer(Long customFieldDefinitionId, String question, Long customFieldElementId, String answer) {
+        this.customFieldDefinitionId = customFieldDefinitionId;
         this.question = question;
+        this.customFieldElementId = customFieldElementId;
         this.answer = answer;
+    }
+
+    public Long getCustomFieldDefinitionId() {
+        return customFieldDefinitionId;
+    }
+
+    public void setCustomFieldDefinitionId(Long customFieldDefinitionId) {
+        this.customFieldDefinitionId = customFieldDefinitionId;
     }
 
     public String getQuestion() {
@@ -41,6 +66,14 @@ public class QuestionAnswer {
 
     public void setQuestion(String question) {
         this.question = question;
+    }
+
+    public Long getCustomFieldElementId() {
+        return customFieldElementId;
+    }
+
+    public void setCustomFieldElementId(Long customFieldElementId) {
+        this.customFieldElementId = customFieldElementId;
     }
 
     public String getAnswer() {
