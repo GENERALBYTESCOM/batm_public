3173c3d7e65012df4f6a23a1124be0c3daeb5e3e
Dominik Roček <dominik.rocek@generalbytes.com>
2024-09-17 17:14:21 +0200

    CF-672: adjustment of javadoc

--- server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/IExtensionContext.java
+++ server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/IExtensionContext.java
@@ -868,9 +868,9 @@ public interface IExtensionContext {
      *
      * @param serialNumber     Terminal or GB Safe serial number.
      * @param customStringName Name of custom string.
-     * @param language         Language in ISO 639-1 standard. For default value, set language to {@code "default"} or leave blank.
-     *                         If no value is found for the specified language, the value for the default language
-     *                         will be returned (if available), otherwise {@code null}.
+     * @param language         Language in the ISO standard (for example 'en' for English, 'de' for German, 'de_CH' for Swiss German etc.).
+     *                         For default value, set language to {@code "default"} or leave blank. If no value is found for the specified
+     *                         language, the value for the default language will be returned (if available), otherwise {@code null}.
      * @return Custom string value from the selected terminal or GB Safe in the selected language. Returns {@code null} if not found.
      */
     default String getCustomStringValue(String serialNumber, String customStringName, String language) {
