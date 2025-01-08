26bb449b109d8e6c6040f740ab07def1dd0d3fd2
Dominik Roček <122279630+drocek@users.noreply.github.com>
2025-01-08 10:25:14 +0100

    BATM-6991: add VASP and Travel Rule Provider identification to WalletTypeEvaluationResult.java (#955)

--- server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/travelrule/WalletTypeEvaluationResult.java
+++ server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/travelrule/WalletTypeEvaluationResult.java
@@ -1,5 +1,5 @@
 /*************************************************************************************
- * Copyright (C) 2014-2024 GENERAL BYTES s.r.o. All rights reserved.
+ * Copyright (C) 2014-2025 GENERAL BYTES s.r.o. All rights reserved.
  *
  * This software may be distributed and modified under the terms of the GNU
  * General Public License version 2 (GPL2) as published by the Free Software
@@ -27,10 +27,18 @@ public class WalletTypeEvaluationResult {
 
     private final CryptoWalletType walletType;
     private final boolean belongsToIdentity;
+    private final Long travelRuleProviderId;
+    private final String vaspDid;
 
-    private WalletTypeEvaluationResult(CryptoWalletType walletType, boolean belongsToIdentity) {
+    private WalletTypeEvaluationResult(CryptoWalletType walletType,
+                                       boolean belongsToIdentity,
+                                       Long travelRuleProviderId,
+                                       String vaspDid
+    ) {
         this.walletType = walletType;
         this.belongsToIdentity = belongsToIdentity;
+        this.travelRuleProviderId = travelRuleProviderId;
+        this.vaspDid = vaspDid;
     }
 
     /**
@@ -50,19 +58,66 @@ public class WalletTypeEvaluationResult {
     }
 
     /**
+     * @return Get the VASP DID (decentralized identifier) of wallet owner.
+     */
+    public String getVaspDid() {
+        return vaspDid;
+    }
+
+    /**
+     * @return Get ID of the used Travel Rule Provider.
+     */
+    public Long getTravelRuleProviderId() {
+        return travelRuleProviderId;
+    }
+
+    /**
      * Create a {@link WalletTypeEvaluationResult} for cases where a wallet type
      * is successfully evaluated.
      *
-     * @param walletType The {@link CryptoWalletType} of the evaluated wallet.
+     * @param walletType        The {@link CryptoWalletType} of the evaluated wallet.
      * @param belongsToIdentity True if the wallet belongs to the provided identity, false otherwise.
      * @return The new {@link WalletTypeEvaluationResult}.
      * @throws IllegalArgumentException If the walletType is null.
      */
     public static WalletTypeEvaluationResult evaluated(CryptoWalletType walletType, boolean belongsToIdentity) {
+        return evaluated(walletType, belongsToIdentity, null, null);
+    }
+
+    /**
+     * Create a {@link WalletTypeEvaluationResult} for cases where a wallet type
+     * is successfully evaluated.
+     *
+     * @param walletType           The {@link CryptoWalletType} of the evaluated wallet.
+     * @param belongsToIdentity    True if the wallet belongs to the provided identity, false otherwise.
+     * @param travelRuleProviderId ID of the used Travel Rule Provider.
+     * @param vaspDid              VASP DID (decentralized identifier) of wallet owner.
+     * @return The new {@link WalletTypeEvaluationResult}.
+     * @throws IllegalArgumentException If the walletType is null.
+     */
+    public static WalletTypeEvaluationResult evaluated(CryptoWalletType walletType,
+                                                       boolean belongsToIdentity,
+                                                       Long travelRuleProviderId,
+                                                       String vaspDid
+    ) {
+        validateEvaluatedResult(walletType, travelRuleProviderId, vaspDid);
+        return new WalletTypeEvaluationResult(walletType, belongsToIdentity, travelRuleProviderId, vaspDid);
+    }
+
+    private static void validateEvaluatedResult(CryptoWalletType walletType, Long travelRuleProviderId, String vaspDid) {
         if (walletType == null) {
             throw new IllegalArgumentException("walletType cannot be null");
         }
-        return new WalletTypeEvaluationResult(walletType, belongsToIdentity);
+
+        if (walletType == CryptoWalletType.CUSTODIAL) {
+            if (travelRuleProviderId == null) {
+                throw new IllegalArgumentException("travelRuleProviderId cannot be null for custodial wallet");
+            }
+
+            if (vaspDid == null || vaspDid.isEmpty()) {
+                throw new IllegalArgumentException("vaspDid cannot be blank for custodial wallet");
+            }
+        }
     }
 
     /**
@@ -72,7 +127,7 @@ public class WalletTypeEvaluationResult {
      * @return The new {@link WalletTypeEvaluationResult}.
      */
     public static WalletTypeEvaluationResult unknown() {
-        return new WalletTypeEvaluationResult(CryptoWalletType.UNKNOWN, false);
+        return new WalletTypeEvaluationResult(CryptoWalletType.UNKNOWN, false, null, null);
     }
 
 }
