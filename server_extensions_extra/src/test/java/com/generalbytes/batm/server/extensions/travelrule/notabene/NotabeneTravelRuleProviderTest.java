a72c0cdd518fc8434cb1f1f485193c1da4956487
tplansky <tomas.plansky@generalbytes.com>
2025-03-06 16:06:37 +0100

    BATM-7234 Also test with both clientId and clientSecret being null

--- server_extensions_extra/src/test/java/com/generalbytes/batm/server/extensions/travelrule/notabene/NotabeneTravelRuleProviderTest.java
+++ server_extensions_extra/src/test/java/com/generalbytes/batm/server/extensions/travelrule/notabene/NotabeneTravelRuleProviderTest.java
@@ -359,6 +359,7 @@ class NotabeneTravelRuleProviderTest {
 
     private static Stream<Arguments> provideDifferentCredentials() {
         return Stream.of(
+            Arguments.arguments(null, null),
             Arguments.arguments("clientId", null),
             Arguments.arguments("clientId", "differentClientSecret"),
             Arguments.arguments(null, "clientSecret"),
