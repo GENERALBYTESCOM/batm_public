4d0d81f523038039839221adee5f6dbaf836eb75
Tomáš Plánský <67519977+d0by1@users.noreply.github.com>
2025-05-21 14:21:14 +0200

    BATM-7282 Save transactions hashes for NANO (#1003)

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/common/PollingPaymentSupport.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/common/PollingPaymentSupport.java
@@ -26,7 +26,6 @@ import com.generalbytes.batm.server.extensions.payment.PaymentRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
---
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/nano/rpc/RpcException.java
@@ -0,0 +1,7 @@
+package com.generalbytes.batm.server.extensions.extra.nano.rpc;
+
+public class RpcException extends Exception {
+    public RpcException(String message) {
+        super(message);
+    }
+}
