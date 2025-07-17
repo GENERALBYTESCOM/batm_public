e93601f8117d92b1b84b03209de65038927ca3bc
Dominik Roček <122279630+drocek@users.noreply.github.com>
2025-07-17 12:50:27 +0200

    BATM-6881: refactor BitGo classes (#1034)

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/bitcoin/wallets/bitgo/v2/dto/BitGoTransfersResponse.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/bitcoin/wallets/bitgo/v2/dto/BitGoTransfersResponse.java
@@ -2,17 +2,13 @@ package com.generalbytes.batm.server.extensions.extra.bitcoin.wallets.bitgo.v2.d
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
 public class BitGoTransfersResponse {
-
     @JsonProperty("transfers")
     private List<BitGoTransfer> transfers;
-
-    public List<BitGoTransfer> getTransfers() {
-        return transfers;
-    }
-
 }
