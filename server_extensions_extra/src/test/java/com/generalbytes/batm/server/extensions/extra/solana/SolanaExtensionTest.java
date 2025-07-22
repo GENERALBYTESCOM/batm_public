e98473332c6214a94a109730fb64c84da036edee
Dominik Roček <122279630+drocek@users.noreply.github.com>
2025-07-22 16:47:33 +0200

    BATM-6881: Define USDC (Solana) cryptocurrency, add demo wallet supporting it (#1038)

--- server_extensions_extra/src/test/java/com/generalbytes/batm/server/extensions/extra/solana/SolanaExtensionTest.java
+++ server_extensions_extra/src/test/java/com/generalbytes/batm/server/extensions/extra/solana/SolanaExtensionTest.java
@@ -8,15 +8,20 @@ import com.generalbytes.batm.server.extensions.IRateSource;
 import com.generalbytes.batm.server.extensions.IWallet;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class SolanaExtensionTest {
 
@@ -30,28 +35,38 @@ class SolanaExtensionTest {
     }
 
     @ParameterizedTest
-    @EnumSource(CryptoCurrency.class)
+    @EnumSource(value = CryptoCurrency.class, mode = EnumSource.Mode.INCLUDE, names = {"SOL", "USDCSOL"})
     void testCreateAddressValidator(CryptoCurrency cryptocurrency) {
         ICryptoAddressValidator validator = solanaExtension.createAddressValidator(cryptocurrency.code);
 
-        if (cryptocurrency == CryptoCurrency.SOL) {
-            assertInstanceOf(SolanaAddressValidator.class, validator);
-        } else {
-            assertNull(validator);
-        }
+        assertInstanceOf(SolanaAddressValidator.class, validator);
     }
 
-    @Test
-    void testCreateWallet_dummyWallet() {
-        String walletLogin = "soldemo:CZK:Gk4PLZ3p6wA4CD3shJ8XvsyhFvABvNPKM9Z8ZBrkDWiB";
+    @ParameterizedTest
+    @EnumSource(value = CryptoCurrency.class, mode = EnumSource.Mode.EXCLUDE, names = {"SOL", "USDCSOL"})
+    void testCreateAddressValidator_unsupported(CryptoCurrency cryptocurrency) {
+        ICryptoAddressValidator validator = solanaExtension.createAddressValidator(cryptocurrency.code);
 
+        assertNull(validator);
+    }
+
+    private static Stream<Arguments> testCreateWallet_dummyWallet_arguments() {
+        return Stream.of(
+            arguments("soldemo:CZK:Gk4PLZ3p6wA4CD3shJ8XvsyhFvABvNPKM9Z8ZBrkDWiB", "SOL"),
+            arguments("usdcsoldemo:CZK:Gk4PLZ3p6wA4CD3shJ8XvsyhFvABvNPKM9Z8ZBrkDWiB", "USDCSOL")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testCreateWallet_dummyWallet_arguments")
+    void testCreateWallet_dummyWallet(String walletLogin, String expectedWalletCryptocurrency) {
         IWallet wallet = solanaExtension.createWallet(walletLogin, null);
 
         assertInstanceOf(DummyExchangeAndWalletAndSource.class, wallet);
         if (wallet instanceof DummyExchangeAndWalletAndSource dummyWallet) {
-            assertEquals("SOL", dummyWallet.getPreferredCryptoCurrency());
+            assertEquals(expectedWalletCryptocurrency, dummyWallet.getPreferredCryptoCurrency());
             assertEquals("CZK", dummyWallet.getPreferredFiatCurrency());
-            assertEquals("Gk4PLZ3p6wA4CD3shJ8XvsyhFvABvNPKM9Z8ZBrkDWiB", dummyWallet.getCryptoAddress("SOL"));
+            assertEquals("Gk4PLZ3p6wA4CD3shJ8XvsyhFvABvNPKM9Z8ZBrkDWiB", dummyWallet.getCryptoAddress(expectedWalletCryptocurrency));
         }
     }
 
@@ -75,25 +90,37 @@ class SolanaExtensionTest {
         assertNull(wallet);
     }
 
-    @Test
-    void testCreateRateSource_fixPriceRateSource() {
-        String sourceLogin = "solfix:21:CZK";
+    private static Stream<Arguments> testCreateRateSource_fixPriceRateSource_arguments() {
+        return Stream.of(
+            arguments("solfix:21:CZK", "SOL"),
+            arguments("usdcsolfix:21:CZK", "USDCSOL")
+        );
+    }
 
+    @ParameterizedTest
+    @MethodSource("testCreateRateSource_fixPriceRateSource_arguments")
+    void testCreateRateSource_fixPriceRateSource(String sourceLogin, String cryptocurrency) {
         IRateSource rateSource = solanaExtension.createRateSource(sourceLogin);
 
         assertInstanceOf(FixPriceRateSource.class, rateSource);
-        assertEquals(BigDecimal.valueOf(21L), rateSource.getExchangeRateLast("SOL", "CZK"));
+        assertEquals(BigDecimal.valueOf(21L), rateSource.getExchangeRateLast(cryptocurrency, "CZK"));
         assertEquals("CZK", rateSource.getPreferredFiatCurrency());
     }
 
-    @Test
-    void testCreateRateSource_fixPriceRateSource_defaultValues() {
-        String sourceLogin = "solfix";
+    private static Stream<Arguments> testCreateRateSource_fixPriceRateSource_defaultValues_arguments() {
+        return Stream.of(
+            arguments("solfix", "SOL"),
+            arguments("usdcsolfix", "USDCSOL")
+        );
+    }
 
+    @ParameterizedTest
+    @MethodSource("testCreateRateSource_fixPriceRateSource_defaultValues_arguments")
+    void testCreateRateSource_fixPriceRateSource_defaultValues(String sourceLogin, String cryptocurrency) {
         IRateSource rateSource = solanaExtension.createRateSource(sourceLogin);
 
         assertInstanceOf(FixPriceRateSource.class, rateSource);
-        assertEquals(BigDecimal.ZERO, rateSource.getExchangeRateLast("SOL", "USD"));
+        assertEquals(BigDecimal.ZERO, rateSource.getExchangeRateLast(cryptocurrency, "USD"));
         assertEquals("USD", rateSource.getPreferredFiatCurrency());
     }
 
@@ -120,7 +147,9 @@ class SolanaExtensionTest {
     void testGetSupportedCryptoCurrencies() {
         Set<String> supportedCryptocurrencies = solanaExtension.getSupportedCryptoCurrencies();
 
-        assertEquals(Set.of("SOL"), supportedCryptocurrencies);
+        assertEquals(2, supportedCryptocurrencies.size());
+        assertTrue(supportedCryptocurrencies.contains("SOL"));
+        assertTrue(supportedCryptocurrencies.contains("USDCSOL"));
     }
 
 }
