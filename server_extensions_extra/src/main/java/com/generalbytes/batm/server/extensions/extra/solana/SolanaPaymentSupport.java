7fd979bacb798a85597edc21f7314db6d56e9a8c
Dominik Roček <122279630+drocek@users.noreply.github.com>
2025-07-24 11:30:31 +0200

    BATM-6881: support SELL of Solana via BitGo (#1041)

---
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/solana/SolanaPaymentSupport.java
@@ -0,0 +1,23 @@
+package com.generalbytes.batm.server.extensions.extra.solana;
+
+import com.generalbytes.batm.common.currencies.CryptoCurrency;
+import com.generalbytes.batm.server.extensions.extra.common.QueryableWalletPaymentSupport;
+
+import java.util.concurrent.TimeUnit;
+
+public class SolanaPaymentSupport extends QueryableWalletPaymentSupport {
+    @Override
+    protected String getCryptoCurrency() {
+        return CryptoCurrency.SOL.getCode();
+    }
+
+    @Override
+    protected long getPollingPeriodMillis() {
+        return TimeUnit.SECONDS.toMillis(30);
+    }
+
+    @Override
+    protected long getPollingInitialDelayMillis() {
+        return TimeUnit.SECONDS.toMillis(60);
+    }
+}
