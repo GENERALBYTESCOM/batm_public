f01e0e9706be61a5f928b432bed90c1355724d2a
Karel Kyovsky <karel.kyovsky@generalbytes.com>
2025-08-09 16:40:26 +0200

    Vibecoded support for smsbrana.cz SMS provider. Additionally Nexmo SMS provider is now moved to extensions.extra module.
    Vibocode prompt was:
    
    Implement com.generalbytes.batm.server.extensions.examples.communication.SMSBranaCZProvider communication provider.
    Take as an example implementation NexmoSmsProvider for a different 3rd party service API.
    Try to use alrady in project imported si.mazi.rescu library for communication with the smsbrana.cz API.
    Here is the API documentation: @https://portal.smsbrana.cz/dokumenty/smsconnect_dokumentace_cz_revison2.pdf
    
    Make sure you make provider available via ExternalCommunicationExampleExtension

---
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/communication/ISMSBranaCZAPI.java
@@ -0,0 +1,66 @@
+package com.generalbytes.batm.server.extensions.extra.communication;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+
+/**
+ * SMSBrána.cz API interface using si.mazi.rescu
+ * API documentation: <a href="https://portal.smsbrana.cz/dokumenty/smsconnect_dokumentace_cz_revison2.pdf">here</a>
+ * Note: API returns XML responses in format:
+ * <result>
+ *   <err>0</err>
+ *   <price>1.1</price>
+ *   <sms_count>1</sms_count>
+ *   <credit>1523.32</credit>
+ *   <sms_id>377351</sms_id>
+ * </result>
+ */
+@Produces(MediaType.TEXT_PLAIN)
+@Path("/http.php")
+public interface ISMSBranaCZAPI {
+
+    /**
+     * Send SMS using SMSBrána.cz API with secure authentication
+     *
+     * @param login SMS Connect username
+     * @param time timestamp in format YYYYMMDDTHHMMSS
+     * @param salt random string (max 50 chars), must be unique per day
+     * @param auth MD5 hash of (password + time + salt)
+     * @param action action name, should be "send_sms"
+     * @param number phone number in national or international format
+     * @param message SMS text (max 459 chars, no diacritics)
+     * @return JAXB XML response object from API
+     * @throws IOException if communication fails
+     */
+    @GET
+    String sendSms(
+        @QueryParam("login") String login,
+        @QueryParam("time") String time,
+        @QueryParam("sul") String salt,
+        @QueryParam("auth") String auth,
+        @QueryParam("action") String action,
+        @QueryParam("number") String number,
+        @QueryParam("message") String message
+    ) throws IOException;
+
+    /**
+     * Get credit info using SMSBrána.cz API
+     *
+     * @param login SMS Connect username
+     * @param time timestamp in format YYYYMMDDTHHMMSS
+     * @param salt random string (max 50 chars), must be unique per day
+     * @param auth MD5 hash of (password + time + salt)
+     * @param action action name, should be "credit_info"
+     * @return JAXB XML response object with credit information
+     * @throws IOException if communication fails
+     */
+    @GET
+    String getCreditInfo(
+        @QueryParam("login") String login,
+        @QueryParam("time") String time,
+        @QueryParam("sul") String salt,
+        @QueryParam("auth") String auth,
+        @QueryParam("action") String action
+    ) throws IOException;
+}
