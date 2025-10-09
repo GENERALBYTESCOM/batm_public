997b6a5c10356e180ba7d662155efb7ee52283df
Dominik Roček <dominik.rocek@generalbytes.com>
2025-10-09 16:06:02 +0200

    BATM-7754: copied create method from ISumSubApi.java to SumsubApiFactory.java

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/common/sumsub/api/SumsubApiFactory.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/common/sumsub/api/SumsubApiFactory.java
@@ -1,5 +1,6 @@
 package com.generalbytes.batm.server.extensions.common.sumsub.api;
 
+import com.generalbytes.batm.server.extensions.extra.identityverification.sumsub.api.ISumSubApi;
 import com.generalbytes.batm.server.extensions.travelrule.sumsub.api.SumsubTravelRuleApi;
 import com.generalbytes.batm.server.extensions.common.sumsub.api.digest.SumsubSignatureDigest;
 import com.generalbytes.batm.server.extensions.common.sumsub.api.digest.SumsubTimestampProvider;
@@ -23,6 +24,25 @@ public class SumsubApiFactory {
     private static final String HEADER_APP_TOKEN = "X-App-Token";
     private static final String HEADER_APP_SIG = "X-App-Access-Sig";
     public static final String HEADER_APP_TS = "X-App-Access-Ts";
+
+    /**
+     * Creates an instance of the ISumSubApi interface for interacting with the SumSub API. This method
+     * configures the necessary headers and settings required for API communication, including the
+     * authentication token, timestamp provider, and signature digest.
+     *
+     * @param token           the API token used for authenticating requests to the SumSub API
+     * @param signatureDigest an instance of SumSubSignatureDigest used to generate secure HMAC-SHA256 signatures
+     * @param timestampDigest an instance of SumSubTimestampProvider to provide a timestamp for the requests
+     * @return a configured instance of ISumSubApi ready for sending requests to the SumSub API
+     */
+    static ISumSubApi create(String token, SumsubSignatureDigest signatureDigest, SumsubTimestampProvider timestampDigest) {
+        ClientConfig config = new ClientConfig();
+        config.addDefaultParam(HeaderParam.class, HEADER_APP_TOKEN, token);
+        config.addDefaultParam(HeaderParam.class, HEADER_APP_TS, timestampDigest);
+        config.addDefaultParam(HeaderParam.class, HEADER_APP_SIG, signatureDigest);
+        config.setJacksonObjectMapperFactory(new CustomObjectMapperFactory());
+        return RestProxyFactory.createProxy(ISumSubApi.class, BASE_URL, config);
+    }
 
     /**
      * Creates an instance of the SumsubTravelRuleApi interface for interacting with the Sumsub API.
