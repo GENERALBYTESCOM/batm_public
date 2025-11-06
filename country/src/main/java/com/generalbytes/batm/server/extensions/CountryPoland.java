27d47de89d73a2e5ba0cf3427dbbeb309b927e71
Maros Silady <maros.silady@generalbytes.com>
2025-11-06 15:41:23 +0100

    LC-249 - Add poland voivodeships

--- country/src/main/java/com/generalbytes/batm/server/extensions/CountryPoland.java
+++ country/src/main/java/com/generalbytes/batm/server/extensions/CountryPoland.java
@@ -7,29 +7,29 @@ import lombok.Getter;
  * Poland province (voivodeship) identifiers.
  * <p>
  * Usage e.g.:
- * CountryPoland.MZ.getProvinceName()
- * CountryPoland.valueOf("MZ").getProvinceName()
+ * CountryPoland.PL02.getProvinceName()
+ * CountryPoland.valueOf("PL02").getProvinceName()
  */
 @Getter
 @AllArgsConstructor
 public enum CountryPoland implements CountryRegion {
 
-    WP("WP", "Greater Poland"),
-    KP("KP", "Kuyavia-Pomerania"),
-    MA("MA", "Lesser Poland"),
-    LD("LD", "Łódź"),
-    DS("DS", "Lower Silesia"),
-    LU("LU", "Lublin"),
-    LB("LB", "Lubusz"),
-    MZ("MZ", "Masovia"),
-    OP("OP", "Opole"),
-    PD("PD", "Podlaskie"),
-    PM("PM", "Pomerania"),
-    SL("SL", "Silesia"),
-    PK("PK", "Subcarpathia"),
-    SK("SK", "Świętokrzyskie"),
-    WM("WM", "Warmia-Masuria"),
-    ZP("ZP", "West Pomerania");
+    PL02("PL02", "Lower Silesian"),
+    PL04("PL04", "Kuyavian-Pomeranian"),
+    PL06("PL06", "Lublin"),
+    PL08("PL08", "Lubusz"),
+    PL10("PL10", "Łódź"),
+    PL12("PL12", "Lesser Poland"),
+    PL14("PL14", "Masovian"),
+    PL16("PL16", "Opole"),
+    PL18("PL18", "Subcarpathian"),
+    PL20("PL20", "Podlaskie"),
+    PL22("PL22", "Pomeranian"),
+    PL24("PL24", "Silesian"),
+    PL26("PL26", "Świętokrzyskie"),
+    PL28("PL28", "Warmian-Masurian"),
+    PL30("PL30", "Greater Poland"),
+    PL32("PL32", "West Pomeranian");
 
     private final String iso;
     private final String provinceName;
