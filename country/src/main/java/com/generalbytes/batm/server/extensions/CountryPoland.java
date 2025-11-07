599b0e9726f4d2d22ce3cae09b9029198513b46a
Maroš Silady <38668012+SMaros@users.noreply.github.com>
2025-11-07 12:51:37 +0100

    LC-529 - Add poland voivodeships (#1105)
    * LC-249 - Add poland voivodeships
    Co-authored-by: Maros Silady <maros.silady@generalbytes.com>

---
+++ country/src/main/java/com/generalbytes/batm/server/extensions/CountryPoland.java
@@ -0,0 +1,36 @@
+package com.generalbytes.batm.server.extensions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Poland province (voivodeship) identifiers.
+ * <p>
+ * Usage e.g.:
+ * CountryPoland.PL02.getProvinceName()
+ * CountryPoland.valueOf("PL02").getProvinceName()
+ */
+@Getter
+@AllArgsConstructor
+public enum CountryPoland implements CountryRegion {
+
+    PL02("PL02", "Lower Silesian"),
+    PL04("PL04", "Kuyavian-Pomeranian"),
+    PL06("PL06", "Lublin"),
+    PL08("PL08", "Lubusz"),
+    PL10("PL10", "Łódź"),
+    PL12("PL12", "Lesser Poland"),
+    PL14("PL14", "Masovian"),
+    PL16("PL16", "Opole"),
+    PL18("PL18", "Subcarpathian"),
+    PL20("PL20", "Podlaskie"),
+    PL22("PL22", "Pomeranian"),
+    PL24("PL24", "Silesian"),
+    PL26("PL26", "Świętokrzyskie"),
+    PL28("PL28", "Warmian-Masurian"),
+    PL30("PL30", "Greater Poland"),
+    PL32("PL32", "West Pomeranian");
+
+    private final String iso;
+    private final String provinceName;
+}
