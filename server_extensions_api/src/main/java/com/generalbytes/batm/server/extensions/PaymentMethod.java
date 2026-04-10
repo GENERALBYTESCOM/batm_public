247a196b3308f15aed9ea84306ccbfab60aefab1
tplansky <tomas.plansky@generalbytes.com>
2026-04-10 17:26:22 +0200

    BATM-8265 Add new profit override methods

---
+++ server_extensions_api/src/main/java/com/generalbytes/batm/server/extensions/PaymentMethod.java
@@ -0,0 +1,10 @@
+package com.generalbytes.batm.server.extensions;
+
+/**
+ * Payment method used for a transaction.
+ */
+public enum PaymentMethod {
+    CASH,
+    PAYMENT_CARD,
+    EXTERNAL_PAYMENT
+}
